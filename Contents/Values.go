7bb8765b1020662f3ba55102c80df3b26f896a17
abulfat.masimov <abulfat.masimov@dentsuaegis.com>
2018-07-31 21:00:54 +0100

    Fixing comments

--- Contents/Values.go
+++ Contents/Values.go
@@ -7,14 +7,13 @@ import "fmt"
 
 func main() {
 	//Strings, which can be added together with +.
-
 	fmt.Println("go" + "lang")
-	//Integers and floats.
 
+	//Integers and floats.
 	fmt.Println("1+1 =", 1+1)
 	fmt.Println("7.0/3.0 =", 7.0/3.0)
-	//Booleans, with boolean operators as you’d expect.
 
+	//Booleans, with boolean operators as you’d expect.
 	fmt.Println(true && false)
 	fmt.Println(true || false)
 	fmt.Println(!true)
