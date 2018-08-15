d80b96afd807c468c906185931a44785f3b5aef7
Dumblelord <Fuadmasimov2018@gmail.com>
2018-08-15 18:32:40 +0100

    ,

--- Contents/Maps.go
+++ Contents/Maps.go
@@ -29,7 +29,7 @@ func main() {
 	fmt.Println("map:", m)
 	//The optional second return value when getting a value from a map indicates if the key was present in the map. This can be used to disambiguate between missing keys and keys with zero values like 0 or "". Here we didn’t need the value itself, so we ignored it with the blank identifier _.
 
-	prz, prs := m["k2"]
+	_, prs := m["k2"]
 	fmt.Println("prs:", prs, prz)
 	//You can also declare and initialize a new map in the same line with this syntax.
 
